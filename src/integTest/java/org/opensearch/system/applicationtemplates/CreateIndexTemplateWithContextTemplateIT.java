a8ea4ac7aaf5501d422f51157a9b96402b0348ea
Mohit Godwani <81609427+mgodwan@users.noreply.github.com>
2024-09-03 16:58:04 +0530

    Backport 2.x ::  Onboarding New templates and adding zstd support (#8) (#11)
    * Onboarding New templates and adding zstd support (#8)
    
    * Add new templates with zstd support
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    * Add schema link with version
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    * Trigger Build
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    * Trigger Build
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    ---------
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    * Add version 2.x for artefacts
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    * Modify IT code for using available dependencies
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>
    
    ---------
    
    Signed-off-by: Mohit Godwani <mgodwan@amazon.com>

--- src/integTest/java/org/opensearch/system/applicationtemplates/CreateIndexTemplateWithContextTemplateIT.java
+++ src/integTest/java/org/opensearch/system/applicationtemplates/CreateIndexTemplateWithContextTemplateIT.java
@@ -8,39 +8,71 @@
 
 package org.opensearch.system.applicationtemplates;
 
-import org.apache.hc.client5.http.auth.AuthScope;
-import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
-import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
-import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
-import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
-import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
-import org.apache.hc.core5.function.Factory;
-import org.apache.hc.core5.http.HttpHost;
-import org.apache.hc.core5.http.nio.ssl.TlsStrategy;
-import org.apache.hc.core5.reactor.ssl.TlsDetails;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.opensearch.client.Request;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestClientBuilder;
+import org.opensearch.cluster.metadata.IndexMetadata;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.core.common.Strings;
+import org.opensearch.index.IndexSettings;
+import org.opensearch.index.engine.EngineConfig;
 import org.opensearch.test.rest.OpenSearchRestTestCase;
 
-import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLContext;
 
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 import java.util.Objects;
-
-import static org.opensearch.client.RestClientBuilder.DEFAULT_MAX_CONN_PER_ROUTE;
-import static org.opensearch.client.RestClientBuilder.DEFAULT_MAX_CONN_TOTAL;
 
 public class CreateIndexTemplateWithContextTemplateIT extends OpenSearchRestTestCase {
 
-    public void testCreateIndexWithContextBasedTemplate() throws IOException {
-        // TODO: Add E2E test with rest layer here.
+    @SuppressWarnings("unchecked")
+    public void testCreateIndexWithContextBasedTemplate() throws Exception {
+
+        final String indexTemplate = "my-metrics-template";
+        final String index = "my-metrics-1";
+
+        Request request = new Request("PUT", "/_index_template/" + indexTemplate);
+        String content = "{\n"
+            + "    \"index_patterns\": [\n"
+            + "        \"my-metrics-*\"\n"
+            + "    ],\n"
+            + "    \"context\": {\n"
+            + "        \"name\": \"metrics\"\n"
+            + "    }\n"
+            + "}";
+
+        request.setJsonEntity(content);
+
+        // Create index template
+        client().performRequest(request);
+
+        // creating index
+        createIndex(index, Settings.builder().put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 0).build());
+
+        Map<String, Object> currentIndexSettings = (Map<String, Object>) ((Map<String, Object>) getIndexSettings(index).get(index)).get(
+            "settings"
+        );
+        assertEquals(currentIndexSettings.get(IndexSettings.INDEX_REFRESH_INTERVAL_SETTING.getKey()), "60s");
+        assertEquals(currentIndexSettings.get(IndexSettings.INDEX_MERGE_POLICY.getKey()), "log_byte_size");
+        assertEquals(currentIndexSettings.get(EngineConfig.INDEX_CODEC_SETTING.getKey()), "zstd_no_dict");
+
+        try {
+            ensureGreen(index);
+        } finally {
+            deleteIndex(index);
+        }
     }
 
     @Override
@@ -65,36 +97,20 @@ public class CreateIndexTemplateWithContextTemplateIT extends OpenSearchRestTest
                 throw new RuntimeException("password is missing");
             }
 
-            final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            final AuthScope anyScope = new AuthScope(null, -1);
-            credentialsProvider.setCredentials(anyScope, new UsernamePasswordCredentials(username, password.toCharArray()));
+            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 
             try {
-                final TlsStrategy tlsStrategy = ClientTlsStrategyBuilder.create()
-                    .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                    .setSslContext(SSLContextBuilder.create().loadTrustMaterial(null, (chains, authType) -> true).build())
-                    // See https://issues.apache.org/jira/browse/HTTPCLIENT-2219
-                    .setTlsDetailsFactory(new Factory<SSLEngine, TlsDetails>() {
-                        @Override
-                        public TlsDetails create(final SSLEngine sslEngine) {
-                            return new TlsDetails(sslEngine.getSession(), sslEngine.getApplicationProtocol());
-                        }
-                    })
-                    .build();
+                final SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build();
 
                 builder.setHttpClientConfigCallback(httpClientBuilder -> {
-                    final PoolingAsyncClientConnectionManager connectionManager = PoolingAsyncClientConnectionManagerBuilder.create()
-                        .setMaxConnPerRoute(DEFAULT_MAX_CONN_PER_ROUTE)
-                        .setMaxConnTotal(DEFAULT_MAX_CONN_TOTAL)
-                        .setTlsStrategy(tlsStrategy)
-                        .build();
-
-                    return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider).setConnectionManager(connectionManager);
+                    return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+                        .setSSLContext(sslContext)
+                        .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE);
                 });
             } catch (final NoSuchAlgorithmException | KeyManagementException | KeyStoreException ex) {
                 throw new IOException(ex);
             }
-
         }
     }
 
